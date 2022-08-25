9300307c201aeb6be8b7dd1a22ffcb6a776ec2bb
Sungminlee <user@AL02016064.local>
2022-08-25 21:30:45 +0900

    first commit

---
+++ variables.tf
@@ -0,0 +1,96 @@
+variable "name" {
+  description = "(Required) See the description in the readme"
+  type        = string
+}
+
+variable "description" {
+  description = "(Optional) See the description in the readme"
+  type        = string
+  default     = ""
+}
+
+variable "subnet_id" {
+  description = "(Required) See the description in the readme"
+  type        = string
+}
+
+variable "fee_system_type_code" {
+  description = "(Optional) See the description in the readme"
+  type        = string
+  default     = "MTRAT"
+}
+
+variable "login_key_name" {
+  description = "(Required) See the description in the readme"
+  type        = string
+}
+
+variable "init_script_id" {
+  description = "(Optional) See the description in the readme"
+  type        = string
+  default     = null
+}
+
+variable "server_image_name" {
+  description = "(Required) See the description in the readme"
+  type        = string
+  default     = null
+}
+
+variable "member_server_image_name" {
+  description = "(Required) See the description in the readme"
+  type        = string
+  default     = null
+}
+
+
+variable "product_generation" {
+  description = "(Required) See the description in the readme"
+  type        = string
+}
+
+variable "product_type" {
+  description = "(Required) See the description in the readme"
+  type        = string
+}
+
+variable "product_name" {
+  description = "(Required) See the description in the readme"
+  type        = string
+}
+
+variable "is_associate_public_ip" {
+  description = "See the description in the readme"
+  type        = bool
+  default     = false
+}
+
+variable "is_protect_server_termination" {
+  description = "(Optional) See the description in the readme"
+  type        = bool
+  default     = false
+}
+
+variable "is_encrypted_base_block_storage_volume" {
+  description = "(Optional) See the description in the readme"
+  type        = bool
+  default     = false
+}
+
+variable "default_network_interface" {
+  description = "(Required) See the description in the readme"
+  # type        = map(map(any))
+}
+
+variable "additional_network_interfaces" {
+  description = "(Optional) See the description in the readme"
+  type        = list(any)
+  default     = []
+}
+
+variable "additional_block_storages" {
+  description = "(Optional) See the description in the readme"
+  type        = list(any)
+  default     = []
+}
+
