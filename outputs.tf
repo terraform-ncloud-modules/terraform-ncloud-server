d77a8179dbc186004207de08d56a9dfea06722a5
Sungminlee <user@AL02016064.local>
2022-08-25 21:37:47 +0900

    added output

--- outputs.tf
+++ outputs.tf
@@ -1,35 +1,11 @@
-# output "vpc" {
-#   value = ncloud_vpc.vpc
-# }
+output "server" {
+  value = ncloud_server.server
+}
 
-# output "public_subnet" {
-#   value = ncloud_subnet.public_subnets
-# }
+output "default_nic" {
+  value = ncloud_network_interface.default_nic
+}
 
-# output "private_subnet" {
-#   value = ncloud_subnet.private_subnets
-# }
-
-# output "loadbalancer_subnet" {
-#   value = ncloud_subnet.loadbalancer_subnets
-# }
-
-# output "network_acls" {
-#   value = ncloud_network_acl.network_acls
-# }
-
-# output "deny_allow_groups" {
-#   value = ncloud_network_acl_deny_allow_group.deny_allow_groups
-# }
-
-# output "public_route_tables" {
-#   value = ncloud_route_table.public_route_tables
-# }
-
-# output "private_route_tables" {
-#   value = ncloud_route_table.private_route_tables
-# }
-
-# output "nat_gateways" {
-#   value = ncloud_nat_gateway.nat_gateways
-# }
+output "additional_block_storages" {
+  value = ncloud_block_storage.additional_block_storages
+}
